d8c58a2c1e7d96495750afd863cc78b394ae5bda
Alex Martinez <alexmartinez.mm98@gmail.com>
2024-12-02 12:15:40 -0500

    initial commit

---
+++ learn_snowflake/models/example/snowflake_customers_purchases.sql
@@ -0,0 +1,6 @@
+
+select c.c_custkey, c.c_name, c.c_nationkey, sum(o.o_totalprice) as total_order_price
+from snowflake_sample_data.tpch_sf1.customer c
+left join snowflake_sample_data.tpch_sf1.orders o
+on c_custkey = o.o_custkey
+group by c.c_custkey, c.c_name, c.c_nationkey
